4d0beb7754eaef9b0a41624e695f6adbdc13d95d
Alejandro norden <alejonorden@gmail.com>
2024-10-03 15:52:07 -0500

    mi segunda api con gatitos

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+import { CatGalleryFetch } from './components/CatGalleryFetch';
+
+function App() {
+
+
+  return (
+    <div>
+      <CatGalleryFetch />
+    </div>
+  )
+}
+
+export default App
+
+
